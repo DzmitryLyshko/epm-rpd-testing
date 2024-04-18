34b13b2f03f7e03f144a2f4ad5e4028b4e0f6022
Dzmitry Lyshko <dzmitry.lyshko@coxautoinc.com>
2024-04-18 17:52:47 +0300

    Set up configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,11 +6,13 @@ import { defineConfig, devices } from '@playwright/test';
  */
 // require('dotenv').config();
 
+export const BASE_URL = process.env.CI ? "https://rp.epam.com" : "http://localhost:8080";
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './src/tests',
+  testDir: './tests/ui/pw',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -18,13 +20,13 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    // baseURL: `${BASE_URL}`,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
